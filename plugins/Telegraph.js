0ab8c996f5751334b7d6ff9c3de77e6a21d93851
𝕋𝕆𝔾𝔼 𝕀ℕ𝕌𝕄𝔸𝕂𝕀 <167923490+toge012345@users.noreply.github.com>
2024-07-30 00:23:07 +0100

    𝐒𝐀𝐒𝐀𝐊𝐈-𝐌𝐃

--- plugins/Telegraph.js
+++ plugins/Telegraph.js
@@ -1,21 +1,3 @@
-
-/**
-========================================================
-          |•|
-     SASAKI ░  MD         |•|
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  Apache 2.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : SASAKI-MD                                                                    
- * @author : Slasher-Official <https://github.com/Alp24ni>   
- * @description : SASAKI-MD ,A Multi-functional whatsapp bot.       
- * @version 2.0.1                                                                                             
- ========================================================
- **/
- 
- 
 const { cmd }   = require('../lib');
 const util = require('util');
 const axios = require('axios');
@@ -27,8 +9,7 @@ cmd({
     },
     async(Void, citel,text) => {
  let a = citel.quoted ? citel.quoted.text : citel.text;
-let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=SASAKI-MD+Bot&author_name=SASAKI-Official &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
+let { data } = await axios.get(`https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Secktor-2.0+Bot&author_name=Slasher-Official &content=[%7B"tag":"p","children":["${a.replace(/ /g,'+')}"]%7D]&return_content=true`);
 return citel.reply(`*Paste created on telegraph*\nName:-${util.format(data.result.title)} \nUrl:- ${util.format(data.result.url)}`)
     }
 );
-
